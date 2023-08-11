3ac9fd60ebacc503f0449d19207dd1f55f0476c6
ravikiranMano <kiran@crayond.co>
2023-08-12 05:23:52 +0530

    prettier formated

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import AppRoutes from './routes'
+import React from 'react';
+import AppRoutes from './routes';
 
-function App () {
+function App() {
   return (
     <>
       <AppRoutes />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
