2ef00e3479222287ab676e5ee78cce9b3e26e7d0
Ravi LV <proraviki@gmail.com>
2023-12-14 00:17:16 +0530

    config provider added for ant theming

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import AppRoutes from './routes';
+import { ConfigProvider } from 'antd';
 
 function App() {
   return (
-    <>
+    <ConfigProvider
+      // theme={{
+      //   token: {
+      //     colorPrimary: '#0059b2',
+      //     borderRadius: 2,
+
+      //     colorBgContainer: '#f6ffed',
+      //   }
+      // }}
+    >
       <div>
         <AppRoutes />
       </div>
-    </>
+    </ConfigProvider>
   );
 }
 
