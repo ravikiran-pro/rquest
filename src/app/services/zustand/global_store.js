cb511e29b2e0f41d326b6675796efee428d89595
Ravi LV <proraviki@gmail.com>
2023-12-14 00:16:53 +0530

    zustand added persist

--- src/app/services/zustand/global_store.js
+++ src/app/services/zustand/global_store.js
@@ -1,9 +1,16 @@
 import { create } from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware'
 
-const useGlobalStore = create((set) => ({
-  user_data: {},
-  update_user_data: (newUserData) =>
-    set(() => ({ user_data: { ...newUserData } })),
-}));
+const useGlobalStore = create(persist(
+  (set, get) => ({
+    user_data: {},
+    update_user_data: (newUserData) =>
+      set(() => ({ user_data: { ...newUserData } })),
+  }),
+  {
+    name: 'food-storage', // name of the item in the storage (must be unique)
+    storage: createJSONStorage(() => sessionStorage), // (optional) by default, 'localStorage' is used
+  },
+),)
 
 export default useGlobalStore;
