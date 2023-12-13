45984c76283ffc5ca6eb07bce9117c5e825fab93
Ravi LV <proraviki@gmail.com>
2023-12-14 03:20:53 +0530

    vite pwa

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,33 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react';
 
 const { PORT = 3001 } = process.env;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: "Rquest",
+        short_name: "Rquest",
+        description: "Rquest - Connect Everywhere Everything",
+        icons: [
+          {
+            src: '/images/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+      devOptions: {
+        enabled: true
+      }
+    })
+  ],
   server: {
     proxy: {
       '/api': {
